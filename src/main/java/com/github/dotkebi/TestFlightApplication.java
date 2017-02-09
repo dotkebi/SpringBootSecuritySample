283365ff340900db8eab4a536c0b9d87f029f8a3
myoungjin <dotkebi@gmail.com>
2017-02-09 14:17:25 +0900

    add quartz module

--- src/main/java/com/github/dotkebi/TestFlightApplication.java
+++ src/main/java/com/github/dotkebi/TestFlightApplication.java
@@ -1,9 +1,12 @@
 package com.github.dotkebi;
 
+import com.github.dotkebi.config.SchedulerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({SchedulerConfig.class})
 public class TestFlightApplication {
 
     public static void main(String[] args) {
